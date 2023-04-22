f18b65007062bab616a282a34135d16f356f3d21
Natjimor <Jimenezorozconatalia@gmail.com>
2023-04-21 23:58:16 -0500

    stars

--- Index.js
+++ Index.js
@@ -1,11 +1,16 @@
 class Movie {
-    constructor (name, rating, description, imagen_f, imagen_L, clasification, stars, stars_no) {
+    constructor (name, rating, description, imagen_f, imagen_L, clasification, star1, star2, star3, star4, star5) {
         this.name = name
         this.rating = rating
         this.description = description
         this.imagen_f = imagen_f
         this.imagen_L = imagen_L
         this.clasification = clasification
+        this.star1 = star1
+        this.star2 = star2
+        this.star3 = star3
+        this.star4 = star4
+        this.star5 = star5
     }
 
     toRecomended(id) {
@@ -16,11 +21,11 @@ class Movie {
             <h4><img id="Play_Icon" src="baseline_play_arrow_white_48dp.png" alt="Play">Play</h4>
             <div class="Información">
                 <p>Ratings: </p>
-                <img class="Tamaño" src="yellow_fill_star.png" alt="Estrella">
-                <img class="Tamaño" src="yellow_fill_star.png" alt="Estrella">
-                <img class="Tamaño" src="yellow_fill_star.png" alt="Estrella">
-                <img class="Tamaño" src="yellow_fill_star.png" alt="Estrella">
-                <img class="Tamaño" src="yellow_outline_star.png" alt="Estrella vacía">
+                <img class="Tamaño" src="${this.star1}" alt="">
+                <img class="Tamaño" src="${this.star2}" alt="">
+                <img class="Tamaño" src="${this.star3}" alt="">
+                <img class="Tamaño" src="${this.star4}" alt="">
+                <img class="Tamaño" src="${this.star5}" alt="">
                 <img class="Tamaño" src="baseline_bookmark_border_white_48dp.png" alt="Guardar">
             </div>
         </div>
